2aa1c8d0e87efe08e9b0b65d938ec3a6dabe3d8b
AlternateReality <metonator@gmail.com>
2022-04-18 15:16:56 +0200

    Update LegitRaceBO.java

--- src/main/java/com/soapboxrace/core/bo/LegitRaceBO.java
+++ src/main/java/com/soapboxrace/core/bo/LegitRaceBO.java
@@ -125,11 +125,13 @@ public class LegitRaceBO {
             sendReport("User drove a Traffic (or nonexistent) Car", activePersonaId, arbitrationPacket);
         }
 
-        if(sessionEntity.getEvent().getCarClassHash() == 607077938 && carEntity.getCarClassHash() != sessionEntity.getEvent().getCarClassHash()) {
-            reportMessage = String.format("User drove a car that doesn't meet the class restriction of the event (class %s, eventname %s).", 
-                HelpingTools.getClass(carEntity.getCarClassHash()), eventName);
+        if(sessionEntity.getEvent().getCarClassHash() == 607077938) {
+            if(carEntity.getCarClassHash() != sessionEntity.getEvent().getCarClassHash()) {
+                reportMessage = String.format("User drove a car that doesn't meet the class restriction of the event (class %s => &s, eventname %s).", 
+                    HelpingTools.getClass(carEntity.getCarClassHash()), HelpingTools.getClass(sessionEntity.getEvent().getCarClassHash()), eventName);
 
-            sendReport(reportMessage, activePersonaId, arbitrationPacket);
+                sendReport(reportMessage, activePersonaId, arbitrationPacket);
+            }
         }
 
         return true;
